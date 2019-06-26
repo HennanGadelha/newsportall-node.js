cc7c66879abbd2dfd590301e512e5d1f38c0e52a
Hennan Gadelha <hennanhennan@gmail.com>
2019-06-25 23:18:03 -0300

    refatorando rota admin

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,7 +2,8 @@ const { check, validationResult } = require('express-validator');
 module.exports = (app) => {
 
     app.get('/addnoticia', (req, res)=>{
-        res.render('admin/form_add_noticia');
+        
+        app.app.controllers.admin.addNoticia(app, req, res);
     })
 
     app.post('/noticias/salvar',[
@@ -19,30 +20,15 @@ module.exports = (app) => {
 
         ], (req, res)=>{
         
-        let noticia = req.body
-        
-        const errors = validationResult(req);
 
-        
+            const errors = validationResult(req);
 
-        console.log(req);
-        
+            if (!errors.isEmpty()) {
+                
+                return res.status(422).json({ errors: errors.array() });
+            }
 
-        if (!errors.isEmpty()) {
-            
-            
-            return res.status(422).json({ errors: errors.array() });
-
-        }
-
-        let conn = app.config.dbConnection();
-        let noticiasModel = new app.app.models.NoticiasDAO(conn);
-
-        noticiasModel.salvarNoticia(noticia,(error, result)=>{
-    
-            res.redirect('/noticias');
-            
-        });
+            app.app.controllers.admin.salvarNoticia(app, req, res);
         
     })
 }
