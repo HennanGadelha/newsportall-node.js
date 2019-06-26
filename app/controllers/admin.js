00cfbd01889d94ea730517039c483a876f2bf200
Hennan Gadelha <hennanhennan@gmail.com>
2019-06-25 23:14:15 -0300

    implementando controllers

---
+++ app/controllers/admin.js
@@ -0,0 +1,25 @@
+module.exports.addNoticia = function(app, req, res){
+
+    res.render('admin/form_add_noticia');
+
+}
+
+
+module.exports.salvarNoticia = function(app, req,res){
+
+    let noticia = req.body
+        
+   
+
+    let conn = app.config.dbConnection();
+    let noticiasModel = new app.app.models.NoticiasDAO(conn);
+
+    noticiasModel.salvarNoticia(noticia,(error, result)=>{
+
+        res.redirect('/noticias');
+        
+    });
+
+
+
+}
