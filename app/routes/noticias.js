2740e6cd3b0081f4352cf181cb98833a25df48d6
Hennan Gadelha <hennanhennan@gmail.com>
2019-06-25 23:19:25 -0300

    refatorando rota de noticias

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -12,6 +12,17 @@ module.exports = (app)=>{
         });
        
     })
+
+    app.get('/noticia', (req, res)=>{
+
+        let conn = app.config.dbConnection();
+        let noticiasModel = new app.app.models.NoticiasDAO(conn);
+
+        noticiasModel.getNoticia((error, result)=>{
+            res.render('noticias/noticia', {noticia: result});
+        })
+    })
+
 }
 
 
